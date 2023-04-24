500ed9d68b5384daa9650cc6e10e796b17b29e9d
Aaron-Yu1 <peizhi.yu@hotmail.com>
2023-04-24 15:04:22 +0800

    update StartApplication func

--- bookstore_users-api/controllers/users_controller.go
+++ bookstore_users-api/controllers/users_controller.go
@@ -1,1 +1,9 @@
 package controllers
+
+func GetUser() {
+
+}
+
+func CreateUser() {
+
+}
--- bookstore_users-api/main.go
+++ bookstore_users-api/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Aaron-Yu1/Go-Test\bookstore_users-api\app"
+	"github.com/Aaron-Yu1/Go-Test/bookstore_users-api/app"
 )
 
 func main() {
